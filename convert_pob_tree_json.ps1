8044fcbdb4e9601e4ba82a81f51297e87e58dbc7
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2021-04-17 13:28:44 +0100

    Add utility to convert passive skill tree from POB

---
+++ convert_pob_tree_json.ps1
@@ -0,0 +1,18 @@
+﻿# This converts POB's tree.lua into a workable JSON - it's a massive kludge but it works
+# It assumes you've installed a full POB into the pob subdir - or at least the tree file!
+
+$POBTREEVER = "3_14"
+
+$POBFile = "$PSScriptRoot/pob/treedata/" + $POBTREEVER + "/tree.lua"
+
+$luadata = Get-Content $POBFile -raw
+
+$luadata = $luadata -replace "^return\w*" , ""
+$luadata = $luadata -replace '\["*([^"]*)"*\]\w*=' , '"$1":'
+$luadata = $luadata -replace '[^"]:' , ''
+$luadata = $luadata -replace '(?ms){([^:]*?)}' , '[$1]'
+$luadata = $luadata -replace '(?ms),[^"]*"groups":.*' , '}'
+
+$luadata | Set-Content "$PSScriptRoot/passive-skill-tree.json"
+
+# TEST $jsondata = Get-Content "$PSScriptRoot/passive-skill-tree.json" | Out-String | ConvertFrom-Json 
